ae1a907b13efd795f73573590a7fb7ecf0d5a61e
VitaliyKaramanuca <Vitalik_Karamanuca>
2024-04-09 20:09:00 +0300

    MENU AND ACCORDEON

--- js/main.js
+++ js/main.js
@@ -11,3 +11,10 @@ const swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
       },
   });
+
+
+  document.querySelectorAll('.accordeon__triger').forEach((item) => {
+    item.addEventListener('click', () => {
+      item.parentNode.classList.toggle('accordeon__item--active')
+    })
+  });
